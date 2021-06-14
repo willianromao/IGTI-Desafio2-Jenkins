996278560abbe4de85d132b42d621ced7742b4b6
Willian Romao <willian.romao@outlook.com.br>
2021-06-14 15:39:10 -0300

    Azure Service Principal

--- automation/main.tf
+++ automation/main.tf
@@ -11,6 +11,10 @@ terraform {
 
 provider "azurerm" {
   features {}
+  subscription_id = "$AZURE_SUBSCRIPTION_ID"
+  client_id       = "$AZURE_CLIENT_ID"
+  client_secret   = "$AZURE_CLIENT_SECRET"
+  tenant_id       = "$AZURE_TENANT_ID"
 }
 
 resource "azurerm_resource_group" "rg" {
